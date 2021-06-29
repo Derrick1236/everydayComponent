fd6fcadf433d45594ed945b3716317359177457b
Derrick1236 <sbz7drk@gmail.com>
2021-06-29 21:57:06 +1000

    add comment in js file

--- js/change_icon.js
+++ js/change_icon.js
@@ -1,4 +1,4 @@
-
+//TODO: Press the following button to change the icon.
 
 function cat_icon_change() {
     document.getElementById("petIcon").src="/Users/derricksong/Desktop/每天一个小组件/everydayComponent/img/cat_icon.svg";

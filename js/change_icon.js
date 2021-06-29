03b88215379d6a9c99d5c9621c580963bce3c1bf
Derrick1236 <sbz7drk@gmail.com>
2021-06-29 21:53:19 +1000

    finishing image changing program

---
+++ js/change_icon.js
@@ -0,0 +1,9 @@
+
+
+function cat_icon_change() {
+    document.getElementById("petIcon").src="/Users/derricksong/Desktop/每天一个小组件/everydayComponent/img/cat_icon.svg";
+}
+
+function dog_icon_change() {
+    document.getElementById("petIcon").src="/Users/derricksong/Desktop/每天一个小组件/everydayComponent/img/dog_icon.svg";
+}

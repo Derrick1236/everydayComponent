c51e580e9ee99bccc0c255a281f3d616e967ff56
Derrick1236 <sbz7drk@gmail.com>
2021-07-01 19:35:57 +1000

    finish html in js

---
+++ js/htmlInJs.js
@@ -0,0 +1,18 @@
+
+  function test() {
+    //const text = "name:mike;age:16;gender:male";
+    const text = "name:mira;age:16";
+    let raw_array = text.split(';')
+    //return('<strong>' + arr + '</strong>');
+    for (let i in raw_array){
+      let arr = raw_array[i].split(':');
+      return('<strong>' + arr[0] + ':</strong>' + ' ' +arr[1] + '\n');
+    }
+  }
+
+  function trigger() {
+      document.querySelector('.txt').innerHTML = test();
+      //console.log(test());
+  }
+//test();
+//trigger();

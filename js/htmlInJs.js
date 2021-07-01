c636640384e14d58f7359f924f07facbdda1fb57
Derrick1236 <sbz7drk@gmail.com>
2021-07-02 09:06:51 +1000

    fix problem

--- js/htmlInJs.js
+++ js/htmlInJs.js
@@ -4,13 +4,14 @@
     //const text = "name:mira;age:16";
     let raw_array = text.split(';')
     //return('<strong>' + arr + '</strong>');
-    for (let i in raw_array){
-      let arr = raw_array[i].split(':');
-      return('<strong>' + arr[0] + ':</strong>' + ' ' +arr[1] + '\n');
+    let res = '';
+    for (i in raw_array){
+      let arr = raw_array[i].split(':') ;
+      res = res.concat('<strong>' + arr[0]+ ':</strong>' + ' ' + arr[1] + '</br>');
     }
+    return res;
   }
 
   function trigger() {
       document.querySelector('.txt').innerHTML = test();
-      console.log(test());
   }
